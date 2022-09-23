30492de5e54f7b66871f2f76b39c19735c51a99a
Ali Hassaine <ali.hs@outlook.fr>
2022-09-23 19:56:07 +0000

    Setup ImageList component

--- src/components/ImageList/ImageList.js
+++ src/components/ImageList/ImageList.js
@@ -1,9 +1,25 @@
 import React from 'react';
-
+import './ImageList.css';
+import cardImage from '../../card-img.jpg';
+import { Link } from 'react-router-dom';
 const ImageList = (props) => {
   return (
     <>
       <h2>This is the image list component</h2>
+      <div className="container">
+        <Link to="1">
+          <div className="list-item">
+            <img src={cardImage} alt="Waldo Playground" />
+            <div className="list-item__info">
+              <p className="list-item__info__title">
+                The Party <br /> <small>Current Record: 0.05s</small>
+                <br />
+                <small>Player: SomePlayerName</small>{' '}
+              </p>
+            </div>
+          </div>
+        </Link>
+      </div>
     </>
   );
 };
