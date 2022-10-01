c6652285b49ce785751a00439d264e1c683ad559
Ali Hassaine <ali.hs@outlook.fr>
2022-10-01 20:02:50 +0000

    Add image data fetching from api

--- src/components/ImageList/ImageList.js
+++ src/components/ImageList/ImageList.js
@@ -1,24 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './ImageList.css';
-import cardImage from '../../card-img.jpg';
 import { Link } from 'react-router-dom';
 const ImageList = (props) => {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/v1/images')
+      .then((response) => response.json())
+      .then((data) => setImages(data))
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
     <>
       <h2>This is the image list component</h2>
       <div className="container">
-        <Link to="1">
-          <div className="list-item">
-            <img src={cardImage} alt="Waldo Playground" />
-            <div className="list-item__info">
-              <p className="list-item__info__title">
-                The Party <br /> <small>Current Record: 0.05s</small>
-                <br />
-                <small>Player: SomePlayerName</small>{' '}
-              </p>
-            </div>
-          </div>
-        </Link>
+        {images.map((image, index) => {
+          return (
+            <Link key={index} to={image.id}>
+              <div className="list-item">
+                <img
+                  src={image.attributes.thumbnail_url}
+                  alt="Waldo Playground"
+                />
+                <div className="list-item__info">
+                  <p className="list-item__info__title">
+                    {image.attributes.title} <br />{' '}
+                    <small>Current Record: 0.05s</small>
+                    <br />
+                    <small>Player: SomePlayerName</small>{' '}
+                  </p>
+                </div>
+              </div>
+            </Link>
+          );
+        })}
       </div>
     </>
   );
